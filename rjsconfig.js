1492db5e14ffbb9a30d1e8b3bd158957d8cd1baa
jonahmania <jmania999@gmail.com>
2015-10-02 14:49:46 -0400

    initial commit

---
+++ rjsconfig.js
@@ -0,0 +1,28 @@
+'use strict';
+/*jshint ignore:start*/
+var require = {
+  scalejs: {
+    extensions: [
+      '<%=ext_name%>'
+    ]
+  },
+  paths: {
+    ace: '../bower_components/ace/lib/ace',
+    requirejs: '../bower_components/requirejs/require',
+    scalejs: '../bower_components/scalejs/dist/scalejs.min',
+    'scalejs.mvvm.views': '../bower_components/scalejs.mvvm/dist/scalejs.mvvm',
+    knockout: '../bower_components/knockout/dist/knockout',
+    'scalejs.mvvm': '../bower_components/scalejs.mvvm/dist/scalejs.mvvm',
+    'knockout.mapping': '../bower_components/knockout.mapping/knockout.mapping',
+    'scalejs.functional': '../bower_components/scalejs.functional/dist/scalejs.functional.min',
+    text: '../bower_components/text/text'
+  },
+  packages: [
+
+  ],
+  shim: {
+
+  }
+};
+/*jshint ignore:end*/
+
